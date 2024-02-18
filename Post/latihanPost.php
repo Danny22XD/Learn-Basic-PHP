dfc469c5dca6c56b329d90e8cc81ffb4fb4b384b
Danny22XD <114897191+Danny22XD@users.noreply.github.com>
2024-02-18 19:40:50 +0700

    POST dan fitur Searching
    memahami kegunaan $_POST dan Menambahkan fitur Searching.

---
+++ Post/latihanPost.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Latihan Post</title>
+</head>
+<body>
+   
+   <h1>Silahkan isi data dibawah</h1>
+
+   <form action="latihanTargetPost.php" method="post">
+      <input type="text" name="nama">
+      <br>
+      <button type="submit" name="submit">Kirim</button>
+   </form>
+</body>
+</html>
---
+++ Searching/functions.php
@@ -0,0 +1,62 @@
+<?php 
+//function untuk connect ke database
+$db = mysqli_connect("localhost", "root", "", "latihanPhp");
+
+//function untuk query database
+function query($query) {
+   global $db;
+   $result = mysqli_query($db, $query);
+   $datas = [];
+   while($data = mysqli_fetch_assoc($result)) {
+      $datas[] = $data;
+   }
+   return $datas;
+}
+
+//function query data pada form untuk dikirimkan ke dalam database
+function create($data) {
+   global $db;
+   //ambil data pada tiap-tiap elemen form
+   $gambar = htmlspecialchars($data["gambar"]);
+   $judul = htmlspecialchars($data["judul"]);
+   $pengarang = htmlspecialchars($data["pengarang"]);
+   $tahunterbit = htmlspecialchars($data["tahunterbit"]);
+
+   $query = "INSERT INTO listBuku VALUES ('', '$gambar', '$judul', '$pengarang', '$tahunterbit')";
+   mysqli_query($db, $query);
+
+   return mysqli_affected_rows($db);
+}
+
+//function tombol untuk menghapus data
+function delete($id) {
+   global $db;
+   $query = "DELETE FROM listBuku WHERE id = $id";
+   mysqli_query($db, $query);
+
+   return mysqli_affected_rows($db);
+}
+
+//function untuk melakukan ubah data
+function edit($data) {
+   global $db;
+   $id = $data["id"];
+   $gambar = htmlspecialchars($data["gambar"]);
+   $judul = htmlspecialchars($data["judul"]);
+   $pengarang = htmlspecialchars($data["pengarang"]);
+   $tahunterbit = htmlspecialchars($data["tahunterbit"]);
+
+   $query = "UPDATE listBuku SET gambar = '$gambar', judul_buku = '$judul', pengarang = '$pengarang', tahun_terbit = '$tahunterbit' WHERE id = $id";
+
+   mysqli_query($db, $query);
+
+   return mysqli_affected_rows($db);
+}
+
+function searching($keyword) {
+   $query = "SELECT * FROM listBuku WHERE judul_buku LIKE '%$keyword%' OR pengarang LIKE '%$keyword%' OR tahun_terbit LIKE '%$keyword%'";
+
+   return query($query);
+}
+
+?>
